8b358a5dd88330d938fc47c41ac80c263ac6fd87
mitchmatovic <mmmatovic99@gmail.com>
2021-08-30 15:29:27 -0700

    new 8/30

--- Homework 1/HW1.py
+++ Homework 1/HW1.py
@@ -1,1 +1,7 @@
-x = 1
+fun = lambda x: (x[0] - x[2])**2 + (x[1] + x[2] - 2)**2 + (x[3] - 1)**2 + (x[4] - 1)**2
+cons = ({'type': 'ineq', 'fun':lambda x: x[0] + 3*x[1]},
+        {'type': 'ineq', 'fun':lambda x: x[2] + x[3] - 2*x[4]},
+        {'type': 'ineq', 'fun':lambda x: -x[0] + 2 * x[1] + 2})
+bnds = ((-10, 10), (-10, 10), (-10,10), (-10, 10), (-10, 10))
+res = minimize(fun, (1,1,1,1), method='SLSQP', bounds='bnds',
+               constraints='cons')
